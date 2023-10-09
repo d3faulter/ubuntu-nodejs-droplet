7fd50d4d4f355a5699d5e6497971aecf7c388dc8
d3faulter <nvfbeck@gmail.com>
2023-10-09 14:43:25 +0200

    simple http req

--- src/app.js
+++ src/app.js
@@ -14,3 +14,33 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+// HTTP req
+
+function makeHttpRequest() {
+  const options = {
+    hostname: 'api.example.com',
+    port: 80,
+    path: '/data',
+    method: 'GET'
+  };
+
+  const req = http.request(options, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      console.log('Response Data:', JSON.parse(data));
+    });
+  });
+
+  req.on('error', (error) => {
+    console.error('Request Error:', error);
+  });
+
+  req.end();
+}
