4576d71671c28edd20ea9d615c3b91fa66c77213
Nicolas V. F. B <79728659+d3faulter@users.noreply.github.com>
2023-10-09 15:17:59 +0200

    Update app.js to server as API

--- src/app.js
+++ src/app.js
@@ -1,44 +1,21 @@
+const express = require('express');
 const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+// Define API endpoint to get users
+app.get('/api/users', (req, res) => {
+  const users = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Jane' }
+  ];
+  res.json(users);
+});
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hej det gode hold\n');
-});
-
-// Function to make an internal HTTP request
-function makeInternalHttpRequest() {
-  const options = {
-    hostname: 'localhost',
-    port: 3000,
-    path: '/',
-    method: 'GET'
-  };
-
-  const req = http.request(options, (res) => {
-    let data = '';
-
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    res.on('end', () => {
-      console.log('Internal Response Data:', data);
-    });
-  });
-
-  req.on('error', (error) => {
-    console.error('Internal Request Error:', error);
-  });
-
-  req.end();
-}
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  // Make an internal HTTP request to the server
-  makeInternalHttpRequest();
 });
